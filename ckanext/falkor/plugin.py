3a316a865747d9a6c36269157f1c39f86c543017
wajones98 <will.jones@byzgen.com>
2024-02-09 14:19:48 +0000

    Add config

--- ckanext/falkor/plugin.py
+++ ckanext/falkor/plugin.py
@@ -20,12 +20,30 @@ class FalkorPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IDomainObjectModification, inherit=True)
     plugins.implements(plugins.IResourceController, inherit=True)
+    plugins.implements(plugins.IConfigurable, inherit=True)
 
     # IConfigurer
     def update_config(self, config_):
         toolkit.add_template_directory(config_, "templates")
         toolkit.add_public_directory(config_, "public")
         toolkit.add_resource("fanstatic", "falkor")
+
+    def configure(self, config):
+        self.config = config
+        config_keys = [
+            "falkor.tenant_id",
+            "falkor.core_api_url",
+            "falkor.admin_api_url",
+            "falkor.auth.endpoint",
+            "falkor.auth.client_id",
+            "falkor.auth.client_secret",
+            "falkor.auth.username",
+            "falkor.auth.password",
+        ]
+
+        for key in config_keys:
+            if not config.get(key):
+                raise Exception(f"{key} not present in configration")
 
     # IResourceController
     def before_show(self, resource_dict):
